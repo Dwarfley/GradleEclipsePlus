fcd6826074dbe0f6600b252af0096907ef2bc9bb
Niklas Johansson <dwarfley.private@gmail.com>
2023-12-24 13:51:14 +0100

    Create project files

---
+++ src/main/java/io/github/dwarfley/gradle/eclipseplus/EclipsePlusPlugin.java
@@ -0,0 +1,12 @@
+package io.github.dwarfley.gradle.eclipseplus;
+
+import org.gradle.api.*;
+
+public class EclipsePlusPlugin implements Plugin<Project> {
+	
+	@Override
+	public void apply(Project pProject){
+		
+	}
+	
+}
